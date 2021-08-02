8b699c4beee2b7d9f70a669c2ada3c1b30bdb5ee
Ahmed Zaheer Dadarkar <ahmed.dadarkar@gmail.com>
2021-08-02 14:07:08 +0530

    Update App Component to use Chat with username in URL

--- client/src/component/App.tsx
+++ client/src/component/App.tsx
@@ -1,11 +1,17 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Chat from "./Chat";
+import Home from "./Home";
 
 export default function App() {
     return (
         <BrowserRouter>
             <Switch>
-                <Route path="/chat">Chat</Route>
-                <Route path="/">Home</Route>
+                <Route path="/chat/:username">
+                    <Chat />
+                </Route>
+                <Route path="/">
+                    <Home />
+                </Route>
             </Switch>
         </BrowserRouter>
     );
